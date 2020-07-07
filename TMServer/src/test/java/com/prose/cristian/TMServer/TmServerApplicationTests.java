8f16818b08784c7469f932479f477ea59c798ef9
Cristian Henrici <henricri@students.zhaw.ch>
2020-07-07 21:40:11 +0200

    first commit

---
+++ TMServer/src/test/java/com/prose/cristian/TMServer/TmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prose.cristian.TMServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
