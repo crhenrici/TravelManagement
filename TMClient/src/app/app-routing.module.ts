2b51781ff43a9eb8455ba116bae91420e6473699
henricri <henricri@students.zhaw.ch>
2020-07-08 17:13:32 +0200

    added travel-list and did first layout of application

--- TMClient/src/app/app-routing.module.ts
+++ TMClient/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { TravelListComponent } from './travel-list/travel-list.component';
+import { TravelEditorComponent } from './travel-editor/travel-editor.component';
+import { AppComponent } from './app.component';
+import { PersonEditorComponent } from './person-editor/person-editor.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'person-edit', component: PersonEditorComponent},
+{ path: 'travel-edit', component: TravelEditorComponent},
+{ path: 'travel-list', component: TravelListComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TMClient/src/app/app.module.ts
+++ TMClient/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonEditorComponent } from './person-editor/person-editor.component';
 import { TravelEditorComponent } from './travel-editor/travel-editor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TravelListComponent } from './travel-list/travel-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonEditorComponent,
-    TravelEditorComponent
+    TravelEditorComponent,
+    TravelListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatFormFieldModule,
+    MatInputModule,
+    NgbModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
